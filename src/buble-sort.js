ab66f6caeae80911ffb22e7e1adec33dc6904c97
Risal Walangadi <risal.walangadi@gmail.com>
2021-07-15 11:50:21 +0800

    optimize bubble sort

--- src/buble-sort.js
+++ src/buble-sort.js
@@ -1,22 +1,35 @@
-var swap = (arr, idx1, idx2) => {
-  // const temp = arr[idx1];
-  // arr[idx1] = arr[idx2]
-  // arr[idx2] = temp
-
-  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-};
-
 // O(n)
 function bubleSort(arr) {
-  // iterasi dari akhir ake awal
+  var swap = (arr, idx1, idx2) => {
+    // const temp = arr[idx1];
+    // arr[idx1] = arr[idx2]
+    // arr[idx2] = temp
+
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // iterasi dari index akhir ke index awal array
   for (let i = arr.length; i > 0; i--) {
+    // variable pengecekan jika tidak terjadi perbuahan posisi item
+    // untuk menghindari array yang hampir berurutan
+    // sehingga tidak melakukan pengecekan pada item yang telah
+    // berurutan penuh
+    let noSwaps = true;
+
     // iterasi dari awal sampai index ke-i - 1
     for (let j = 0; j < i - 1; j++) {
       // jika item index ke-j lebih besar dari item index ke-j + 1
       // tukar posisi
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        noSwaps = false;
       }
+    }
+
+    // jika tidak terjadi perubahan posisi pada item
+    // hentikan iterasi
+    if (noSwaps) {
+      break;
     }
   }
 
--- src/test/buble-sort.test.js
+++ src/test/buble-sort.test.js
@@ -2,6 +2,7 @@ const bubleSort = require('../buble-sort');
 
 describe('Bubble sort', () => {
   it('sort an array ascending', () => {
+    expect(bubleSort([5, 1, 2, 3, 4, 6])).toEqual([1, 2, 3, 4, 5, 6]);
     expect(bubleSort([1, 3, 5, 2, 4])).toEqual([1, 2, 3, 4, 5]);
     expect(bubleSort([6, 3, 5, 2, 4, 1])).toEqual([1, 2, 3, 4, 5, 6]);
     expect(bubleSort([21, 11, 50, 32, 47, 44, 33])).toEqual([
