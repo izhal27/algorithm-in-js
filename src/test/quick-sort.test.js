f34c7c7c2cae4080066ad64200950081781c224c
Risal Walangadi <risal.walangadi@gmail.com>
2021-07-19 11:51:06 +0800

    rename test describe

--- src/test/quick-sort.test.js
+++ src/test/quick-sort.test.js
@@ -1,6 +1,6 @@
 const quickSort = require('../quick-sort');
 
-describe('Merge sort', () => {
+describe('Quick sort', () => {
   it('sort an array ascending', () => {
     expect(quickSort([2, 3, 4, 5, 6, 1])).toEqual([1, 2, 3, 4, 5, 6]);
     expect(quickSort([1, 3, 5, 2, 4])).toEqual([1, 2, 3, 4, 5]);
