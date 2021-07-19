36bf2868852362932559f93aa37b1ae5f90353b8
Risal Walangadi <risal.walangadi@gmail.com>
2021-07-19 11:02:49 +0800

    replace var to const

--- src/buble-sort.js
+++ src/buble-sort.js
@@ -1,6 +1,6 @@
 // O(n²)
 function bubleSort(arr) {
-  var swap = (arr, idx1, idx2) => {
+  const swap = (arr, idx1, idx2) => {
     // const temp = arr[idx1];
     // arr[idx1] = arr[idx2]
     // arr[idx2] = temp
